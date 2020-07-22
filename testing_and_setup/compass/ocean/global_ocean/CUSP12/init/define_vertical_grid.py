2280751f644caa1caeddae5c2f6e7c9cb5133759
Kang Hyun <3hk@mac120237.attlocal.net>
2020-07-22 15:58:34 -0400

    First commit on SI-PGPBiCG

--- testing_and_setup/compass/ocean/global_ocean/CUSP12/init/define_vertical_grid.py
+++ testing_and_setup/compass/ocean/global_ocean/CUSP12/init/define_vertical_grid.py
@@ -1,1 +1,12 @@
-../../../scripts/vertical_grid/define_vertical_grid_64_layers.py
+#!/usr/bin/env python
+
+from make_vertical_grid import create_vertical_grid
+
+# Standard 64 layer vertical grid
+create_vertical_grid(
+    num_vert_levels=64,
+    bottom_depth=6000,
+    min_layer_thickness=2.,
+    max_layer_thickness=210.,
+    plot_vertical_grid=True,
+    outfile='vertical_grid.nc')
--- testing_and_setup/compass/ocean/global_ocean/QU240/init/define_vertical_grid.py
+++ testing_and_setup/compass/ocean/global_ocean/QU240/init/define_vertical_grid.py
@@ -1,1 +1,12 @@
-../../../scripts/vertical_grid/define_vertical_grid_16_layers.py
+#!/usr/bin/env python
+
+from make_vertical_grid import create_vertical_grid
+
+# Standard 16 layer vertical grid
+create_vertical_grid(
+    num_vert_levels=16,
+    bottom_depth=3000,
+    min_layer_thickness=3.,
+    max_layer_thickness=500.,
+    plot_vertical_grid=True,
+    outfile='vertical_grid.nc')
